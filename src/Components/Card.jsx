065725f3a9a7ec2e6a0c54b7ebcf8f9e8bfbdf32
Sagor220 <sagormahedi7@gmail.com>
2026-04-01 14:09:31 +0600

    card tag color changed dynamically.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -36,9 +36,24 @@ const Card = ({ card }) => {
       <div className="card bg-base-100 shadow-sm ">
         <div className="card-body ">
           <span className="text-2xl">{icon}</span>
-          <span className="badge badge-xs badge-warning absolute top-0 right-0 m-2.5">
-            {tag}
-          </span>
+
+          {tag === "Best Seller" ? (
+            <div className="badge badge-soft badge-warning absolute top-0 right-0 m-2.5">
+              Best Seller
+            </div>
+          ) : tag === "Popular" ? (
+            <div className="badge badge-soft badge-error absolute top-0 right-0 m-2.5">
+              Popular
+            </div>
+          ) : tag === "New" ? (
+            <div className="badge badge-soft badge-info absolute top-0 right-0 m-2.5">
+              New
+            </div>
+          ) : (
+            <div className="badge badge-outline absolute top-0 right-0 m-2.5">
+              {tag}
+            </div>
+          )}
           <div className="flex justify-between">
             <h2 className="text-2xl font-semibold">{name}</h2>
           </div>
